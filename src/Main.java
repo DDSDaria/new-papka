9344d5c74df0af41ba59272b868278f7be61347d
DDS Daria <dds.darlin00@gmail.com>
2024-02-18 16:52:30 +0300

    changed adult years

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         record Student (String name, int age) {
-            private static final int adultAge = 18;
+            private static final int adultAge = 21;
             public  boolean isAdult() {
                 return age >= adultAge;
             }
